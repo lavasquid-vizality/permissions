063314b9fb6da6ef25c875f6048a3adf82d1f85d
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2021-11-08 11:27:51 +0000

    Fix patching voice channels

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ export default class extends Plugin {
 
       return res;
     });
-    patch(getModule(m => m.default?.displayName === 'ChannelListVoiceChannelContextMenu' && m.default.name === 'C'), 'default', (args, res) => {
+    patch(getModule(m => m.default?.displayName === 'ChannelListVoiceChannelContextMenu' && m.default.name === 'A'), 'default', (args, res) => {
       const { guild, channel } = args[0];
 
       findInReactTree(res.props.children, m => m.children?.[0].props.id === 'hide-voice-names').children.push(<MenuItem action={() => open(() => <ChannelPermissionsModal guild={guild} channel={channel} description={this.settings.get('ChannelPermissionDescription', defaultSettings.ChannelPermissionDescription)} />)} id={'channel-permissions'} label={'View Permissions'} />);
