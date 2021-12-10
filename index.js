6dbf7d4954989d279aee3f3bc45214541fec68e9
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2021-12-10 09:35:23 +0000

    Fix patches, move placement of some MenuItems

--- index.js
+++ index.js
@@ -22,22 +22,22 @@ export default class extends Plugin {
     patch(getModule(m => m.default?.displayName === 'GuildContextMenu'), 'default', (args, res) => {
       const { guild } = args[0];
 
-      findInReactTree(res.props.children, m => m.children?.[0]?.props.id === 'hide-muted-channels').children.splice(2, 0, <MenuItem action={() => open(() => <GuildPermissionsModal guild={guild} description={this.settings.get('GuildPermissionDescription', defaultSettings.GuildPermissionDescription)} />)} id={'guild-permissions'} label={'View Permissions'} />);
+      findInReactTree(res.props.children, m => m.children?.[0]?.props.id === 'mute-guild').children.unshift(<MenuItem action={() => open(() => <GuildPermissionsModal guild={guild} description={this.settings.get('GuildPermissionDescription', defaultSettings.GuildPermissionDescription)} />)} id={'guild-permissions'} label={'View Permissions'} />);
 
       return res;
     });
 
-    patch(getModule(m => m.default?.displayName === 'ChannelListTextChannelContextMenu' && m.default.name === 'h'), 'default', (args, res) => {
+    patch(getModule(m => m.default?.displayName === 'ChannelListTextChannelContextMenu' && m.default.name === 'S'), 'default', (args, res) => {
       const { guild, channel } = args[0];
 
-      findInReactTree(res.props.children, m => m.children?.[0]?.props.id === 'collapse-category').children.push(<MenuItem action={() => open(() => <ChannelPermissionsModal guild={guild} channel={channel} description={this.settings.get('ChannelPermissionDescription', defaultSettings.ChannelPermissionDescription)} />)} id={'category-permissions'} label={'View Permissions'} />);
+      findInReactTree(res.props.children, m => m.children?.[0]?.props.id === 'mute-channel').children.unshift(<MenuItem action={() => open(() => <ChannelPermissionsModal guild={guild} channel={channel} description={this.settings.get('ChannelPermissionDescription', defaultSettings.ChannelPermissionDescription)} />)} id={'category-permissions'} label={'View Permissions'} />);
 
       return res;
     });
     patch(getModule(m => m.default?.displayName === 'ChannelListTextChannelContextMenu' && m.default.name === 'O'), 'default', (args, res) => {
       const { guild, channel } = args[0];
 
-      findInReactTree(res.props.children, m => m.children?.[m.children.length - 1]?.props.id === 'channel-notifications').children.push(<MenuItem action={() => open(() => <ChannelPermissionsModal guild={guild} channel={channel} description={this.settings.get('ChannelPermissionDescription', defaultSettings.ChannelPermissionDescription)} />)} id={'channel-permissions'} label={'View Permissions'} />);
+      findInReactTree(res.props.children, m => m.children?.[0]?.props.id === 'mute-channel').children.unshift(<MenuItem action={() => open(() => <ChannelPermissionsModal guild={guild} channel={channel} description={this.settings.get('ChannelPermissionDescription', defaultSettings.ChannelPermissionDescription)} />)} id={'channel-permissions'} label={'View Permissions'} />);
 
       return res;
     });
